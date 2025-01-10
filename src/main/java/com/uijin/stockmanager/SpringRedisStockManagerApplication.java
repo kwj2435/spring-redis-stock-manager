60ea6fb1c95ede04ef1133404ab0bcbebd0b6414
김의진(한샘몰백엔드개발팀) <kimuj0035@hanssem.com>
2025-01-10 16:45:57 +0900

    Initial commit

---
+++ src/main/java/com/uijin/stockmanager/SpringRedisStockManagerApplication.java
@@ -0,0 +1,13 @@
+package com.uijin.stockmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisStockManagerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringRedisStockManagerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/uijin/stockmanager/SpringRedisStockManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uijin.stockmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisStockManagerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
