bec3e06163cdac356ea9276e4cbd073912e46874
kimuijin <dmlwls2435@naver.com>
2025-01-12 22:01:23 +0900

    feature/1 Redis 클러스터 구현

--- create-redis-cluster.sh
+++ create-redis-cluster.sh
@@ -14,9 +14,7 @@ NODES=(
 CLUSTER_CREATE_CMD="docker exec -it redis-node-1 redis-cli --cluster create ${NODES[@]} --cluster-replicas 1"
 
 # 클러스터 생성 명령 실행
-echo "Redis 클러스터를 생성하는 중..."
 $CLUSTER_CREATE_CMD
 
 # 클러스터 상태 확인
-echo "클러스터 상태 확인 중..."
 docker exec -it redis-node-1 redis-cli -c cluster info
