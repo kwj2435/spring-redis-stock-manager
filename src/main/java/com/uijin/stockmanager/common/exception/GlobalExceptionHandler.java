d3d0a36b14092baf3429e6efe8737dc737d816f6
김의진(한샘몰백엔드개발팀) <kimuj0035@hanssem.com>
2025-01-16 09:53:20 +0900

    [feature/12] Global Exception 핸들링

--- src/main/java/com/uijin/stockmanager/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/uijin/stockmanager/common/exception/GlobalExceptionHandler.java
@@ -1,16 +1,30 @@
 package com.uijin.stockmanager.common.exception;
 
+import com.uijin.stockmanager.common.enums.ApiExceptionCode;
 import com.uijin.stockmanager.common.model.ApiErrorResponse;
+import com.uijin.stockmanager.common.model.ApiException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
   @ExceptionHandler(value = Exception.class)
   public ResponseEntity<ApiErrorResponse> handleException(Exception exception) {
-    return ResponseEntity.internalServerError().body(new ApiErrorResponse());
+    log.error("An unexpected error occurred", exception);
+    ApiErrorResponse response = ApiErrorResponse.internalServerError();
+
+    return new ResponseEntity<>(response, ApiExceptionCode.ERR_500_10001.getHttpStatus());
   }
 
+  @ExceptionHandler(value = ApiException.class)
+  public ResponseEntity<ApiErrorResponse> handleApiException(ApiException apiException) {
+    log.debug(apiException.getMessage());
+    ApiErrorResponse apiErrorResponse = ApiErrorResponse.from(apiException);
+
+    return new ResponseEntity<>(apiErrorResponse, apiException.getHttpStatus());
+  }
 }
