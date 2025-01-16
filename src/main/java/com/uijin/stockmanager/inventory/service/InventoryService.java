7d6cb006724ba499d737aef900e926d9ed62bca3
김의진(한샘몰백엔드개발팀) <kimuj0035@hanssem.com>
2025-01-16 10:16:12 +0900

    [feature/4] 재고 감소 API 개발

--- src/main/java/com/uijin/stockmanager/inventory/service/InventoryService.java
+++ src/main/java/com/uijin/stockmanager/inventory/service/InventoryService.java
@@ -7,8 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import java.security.InvalidParameterException;
-
 import static com.uijin.stockmanager.external.redis.constant.RedisKeyConstant.INVENTORY_DETAILS_KEY;
 
 @Service
@@ -18,6 +16,30 @@ public class InventoryService {
   private final RedisTemplate<String, Object> redisTemplate;
 
   public InventoryModel.InventoryResponse getInventory(long inventoryId) {
+    InventoryModel.Inventory inventory = getInventoryFromRedis(inventoryId);
+
+    return InventoryModel.InventoryResponse.of(inventoryId, inventory);
+  }
+
+  public InventoryModel.InventoryResponse decreaseInventoryStock(long inventoryId) {
+    InventoryModel.Inventory inventory = getInventoryFromRedis(inventoryId);
+
+    if(inventory.getStockQuantity() == 0) {
+      throw ApiException.from(ApiExceptionCode.ERR_409_10001);
+    }
+
+    redisTemplate.opsForHash().put(
+            INVENTORY_DETAILS_KEY,
+            String.valueOf(inventoryId),
+            inventory.getStockQuantity() - 1
+    );
+    
+    // kafka 재고 DB 동기화 이벤트 발행
+    
+    return InventoryModel.InventoryResponse.of(inventoryId, inventory);
+  }
+
+  private InventoryModel.Inventory getInventoryFromRedis(long inventoryId) {
     InventoryModel.Inventory inventory =
             (InventoryModel.Inventory) redisTemplate.opsForHash()
                     .get(INVENTORY_DETAILS_KEY, String.valueOf(inventoryId));
@@ -25,7 +47,7 @@ public class InventoryService {
     if(inventory == null) {
       throw ApiException.from(ApiExceptionCode.ERR_400_10001);
     }
-
-    return InventoryModel.InventoryResponse.of(inventoryId, inventory);
+    return inventory;
   }
+
 }
