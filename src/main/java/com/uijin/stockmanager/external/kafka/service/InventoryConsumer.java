a7ed41eb75560ab1f3e2136456345767583c21c3
김의진(한샘몰백엔드개발팀) <kimuj0035@hanssem.com>
2025-01-16 10:59:26 +0900

    [feature/5] Kafka 구성

--- src/main/java/com/uijin/stockmanager/external/kafka/service/InventoryConsumer.java
+++ src/main/java/com/uijin/stockmanager/external/kafka/service/InventoryConsumer.java
@@ -2,9 +2,9 @@ package com.uijin.stockmanager.external.kafka.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.uijin.stockmanager.inventory.model.Inventory;
 import com.uijin.stockmanager.inventory.repository.InventoryEntityRepository;
 import lombok.RequiredArgsConstructor;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -13,17 +13,20 @@ import org.springframework.stereotype.Service;
 public class InventoryConsumer {
 
   private final InventoryEntityRepository inventoryEntityRepository;
+  private final ObjectMapper mapper = new ObjectMapper();
 
-  @KafkaListener(topics = "stock-updates", groupId = "stock-sync-group")
-  public void consume(String message) {
-    System.out.println("Received message: " + message);
+  @KafkaListener(topics = "stock-updates")
+  public void consumeStockUpdateEvent(String message) throws JsonProcessingException {
+    Inventory inventory = parseMessageToInventory(message);
+    inventoryEntityRepository.updateInventoryStock(
+            inventory.getInventoryId(),
+            inventory.getStockQuantity()
+    );
 
-    ObjectMapper mapper = new ObjectMapper();
-    try {
-      StockUpdate stockUpdate = mapper.readValue(message, StockUpdate.class);
-      inventoryEntityRepository.updateStock(stockUpdate.getProductId(), stockUpdate.getChange());
-    } catch (JsonProcessingException e) {
-      e.printStackTrace();
-    }
+    // JsonProcessingException 발생시 DLQ로 전달 로직 추가 필요
+  }
+
+  private Inventory parseMessageToInventory(String message) throws JsonProcessingException {
+    return mapper.readValue(message, Inventory.class);
   }
 }
--- src/main/java/com/uijin/stockmanager/inventory/model/Inventory.java
+++ src/main/java/com/uijin/stockmanager/inventory/model/Inventory.java
@@ -8,18 +8,24 @@ import java.io.Serializable;
 
 @Getter
 public class Inventory{
-
+  /** 상품 ID */
+  private final long inventoryId;
   /** 상품 명 */
-  private String productName;
+  private final String productName;
   /** 상품 수량 */
-  private int stockQuantity;
+  private final int stockQuantity;
 
-  private Inventory(String productName, int stockQuantity) {
+  private Inventory(long inventoryId, String productName, int stockQuantity) {
+    this.inventoryId = inventoryId;
     this.productName = productName;
     this.stockQuantity = stockQuantity;
   }
 
   public static Inventory from(InventoryEntity inventory) {
-    return new Inventory(inventory.getProductName(), inventory.getStockQuantity());
+    return new Inventory(
+            inventory.getInventoryId(),
+            inventory.getProductName(),
+            inventory.getStockQuantity()
+    );
   }
 }
